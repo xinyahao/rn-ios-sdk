d317cf01c44fe9e0231d4c7cb52cf00392d1a17f
xinya <xinyahao1@gmail.com>
2023-02-02 14:12:25 +0800

    feat: framework

---
+++ Frameworks/RNSDK.xcframework/ios-arm64_x86_64-simulator/RNSDK.framework/Headers/RNSDK.h
@@ -0,0 +1,18 @@
+//
+//  RNSDK.h
+//  RNSDK
+//
+//  Created by xinya on 2023/2/1.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RNSDK.
+FOUNDATION_EXPORT double RNSDKVersionNumber;
+
+//! Project version string for RNSDK.
+FOUNDATION_EXPORT const unsigned char RNSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RNSDK/PublicHeader.h>
+
+
